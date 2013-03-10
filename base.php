51112751f8ad106dff81108fd4ab73a6d18efe6e
Alastair Aitken <al@fastzen.com>
2013-03-10 16:28:30 -0700

    Added Oddman's Nifty Base model to support Soft deletions, Before save filters and After save filters

--- base.php
+++ base.php
@@ -27,6 +27,8 @@ class Base extends \Eloquent
 	 */
 	public $validation = false;
 
+	public static $soft_delete = true;
+	
 	/**
 	 * Validates model.
 	 *
@@ -45,4 +47,46 @@ class Base extends \Eloquent
 
 		return $this->validation->passes();
 	}
+
+	/**
+	 * Custom method for model deletion - basically
+	 * a soft-delete mechanism. Sets the deleted_at
+	 * field on the model.
+	 */
+	public function delete() {
+		if (static::$soft_delete) {
+			$this->deleted_at = new \DateTime;
+			$this->save();
+		}
+		else {
+			parent::delete();
+		}
+	}
+	
+	/**
+	 * Overwrite the save method so as to provide easy
+	 * access to before and after save methods.
+	 */
+	public function save() {
+		// we save this information before saving, because as soon as the
+		// object is created on the database, this evaluates to true.
+		$exists = $this->exists;
+		$original = $this->original;
+
+		$this->before_save($exists, $original);
+		$result = parent::save();
+		
+		if ($result)
+			$this->after_save($exists, $original);
+
+		return $result;
+	}
+
+	// Default methods for overloading
+	protected function before_save($exists, $original) {}
+
+	// we pass in the new record value because otherwise it
+	// would be impossible to ascertain, after the record has been saved.
+	protected function after_save($exists, $original) {}
+
 }
